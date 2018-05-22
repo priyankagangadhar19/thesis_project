04c7de32419ad6cf955367f06c5cc5ba88cbfa80
Athul AK <athullive@gmail.com>
2018-05-22 17:50:18 +0200

    adding routes
    routes added for home and admin page

--- application/config/routes.php
+++ application/config/routes.php
@@ -50,4 +50,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |		my-controller/my-method	-> my_controller/my_method
 */
 $route['default_controller'] = 'home';
-$route['(:any)'] = 'home/$1';
+$route['404_override'] = '';
+$route['translate_uri_dashes'] = FALSE;
+$route['admin/(:any)'] = 'admin/login/$1';
+$route['admin'] = '/admin/login';
