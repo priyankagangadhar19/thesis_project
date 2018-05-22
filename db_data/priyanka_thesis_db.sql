70914e0bcced1d6a98cb3f113c0d88a1176c4d39
Athul AK <athullive@gmail.com>
2018-05-22 02:26:47 +0200

    primary db_data
    adding db primary design in .sql file.

---
+++ db_data/priyanka_thesis_db.sql
@@ -0,0 +1,193 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.2
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: May 22, 2018 at 02:25 AM
+-- Server version: 10.1.19-MariaDB
+-- PHP Version: 7.0.13
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `priyanka_thesis_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `collected_data`
+--
+
+CREATE TABLE `collected_data` (
+  `id` int(250) NOT NULL,
+  `job_title` varchar(250) NOT NULL,
+  `url` varchar(250) NOT NULL,
+  `requirements` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `job_category`
+--
+
+CREATE TABLE `job_category` (
+  `id` int(250) NOT NULL,
+  `category` varchar(250) NOT NULL,
+  `description` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `job_roles`
+--
+
+CREATE TABLE `job_roles` (
+  `id` int(250) NOT NULL,
+  `category_id` int(250) NOT NULL,
+  `role` varchar(250) NOT NULL,
+  `description` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `req_categ_list`
+--
+
+CREATE TABLE `req_categ_list` (
+  `id` int(250) NOT NULL,
+  `name` varchar(250) NOT NULL,
+  `description` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `req_categ_list`
+--
+
+INSERT INTO `req_categ_list` (`id`, `name`, `description`) VALUES
+(1, 'Education', ''),
+(2, 'OS', ''),
+(3, 'Testing Tools', ''),
+(4, 'SQL', ''),
+(5, 'Issue Tracking Tool', ''),
+(6, 'Unit Tests', ''),
+(7, 'Integration Tests', ''),
+(8, 'Agile management', ''),
+(9, 'Build Server', ''),
+(10, 'Static Code Analysis', ''),
+(11, 'Version Control System', ''),
+(14, 'Certifications', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `req_list`
+--
+
+CREATE TABLE `req_list` (
+  `id` int(250) NOT NULL,
+  `req_categ` varchar(250) NOT NULL,
+  `name` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_details`
+--
+
+CREATE TABLE `user_details` (
+  `id` int(250) NOT NULL,
+  `user_name` varchar(250) NOT NULL,
+  `password` varchar(250) NOT NULL,
+  `name` varchar(250) NOT NULL,
+  `email` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `collected_data`
+--
+ALTER TABLE `collected_data`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `job_category`
+--
+ALTER TABLE `job_category`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `job_roles`
+--
+ALTER TABLE `job_roles`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `req_categ_list`
+--
+ALTER TABLE `req_categ_list`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `req_list`
+--
+ALTER TABLE `req_list`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user_details`
+--
+ALTER TABLE `user_details`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `collected_data`
+--
+ALTER TABLE `collected_data`
+  MODIFY `id` int(250) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `job_category`
+--
+ALTER TABLE `job_category`
+  MODIFY `id` int(250) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `job_roles`
+--
+ALTER TABLE `job_roles`
+  MODIFY `id` int(250) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `req_categ_list`
+--
+ALTER TABLE `req_categ_list`
+  MODIFY `id` int(250) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+--
+-- AUTO_INCREMENT for table `req_list`
+--
+ALTER TABLE `req_list`
+  MODIFY `id` int(250) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `user_details`
+--
+ALTER TABLE `user_details`
+  MODIFY `id` int(250) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
