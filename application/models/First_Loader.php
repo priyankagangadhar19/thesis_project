cd9232fa95704ae497b963a15f957ca8377c8a4c
Athul AK <athullive@gmail.com>
2018-05-22 22:41:02 +0200

    model to load necessary classes

--- application/models/First_Loader.php
+++ application/models/First_Loader.php
@@ -3,6 +3,7 @@ class First_Loader extends CI_Model {
     
     function __construct() {
         parent::__construct();
+        $this->load->library('session');
         $this->load->helper('url');
         $this->load->database();
     } 
