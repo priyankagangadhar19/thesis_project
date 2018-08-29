0e1b094e9a9a3552534c73b911239c5a3eaef6d9
Priyanka <priyankagangadhar19@gmail.com>
2018-08-29 12:21:42 +0530

    Views

--- application/views/admin/dashboard.php
+++ application/views/admin/dashboard.php
@@ -1,3 +1,7 @@
+<?php 
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+?>
 <!DOCTYPE html>
 <html>
   <head>
@@ -49,23 +53,23 @@
         </tr>
           <tr>
             <td>Job Category</td>
-            <td><a href="<?php echo base_url()."/admin/jobcateg";?>">link</a></td>
+            <td><a href="<?php echo base_url()."admin/jobcateg";?>">link</a></td>
           </tr>
           <tr>
             <td>Job Role</td>
-            <td><a href="<?php echo base_url()."/admin/jobroles";?>">link</a></td>
+            <td><a href="<?php echo base_url()."admin/jobroles";?>">link</a></td>
           </tr>
           <tr>
             <td>Req Categ List</td>
-            <td><a href="<?php echo base_url()."/admin/reqcateglist";?>">link</a></td>
+            <td><a href="<?php echo base_url()."admin/reqcateglist";?>">link</a></td>
           </tr>
           <tr>
             <td>Req List</td>
-            <td><a href="<?php echo base_url()."/admin/reqlist";?>">link</a></td>
+            <td><a href="<?php echo base_url()."admin/reqlist";?>">link</a></td>
           </tr>
           <tr>
             <td>Add Data</td>
-            <td><a href="<?php echo base_url()."/admin/adddata";?>">link</a></td>
+            <td><a href="<?php echo base_url()."admin/adddata";?>">link</a></td>
           </tr>
       </table>
     </div>
